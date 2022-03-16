6ce017d153148b451cbd34cf6a70b69c7b79bf7a
joaonunes833 <joaonunes833@hotmail.com>
2022-03-16 18:53:46 +0000

    ficha 02 update

--- Ficha02/ficha02.js
+++ Ficha02/ficha02.js
@@ -38,27 +38,87 @@ console.log(reversedWords('Hoje e Domingo'))
 // Função Número de vogais
 
 function numberOfVowels(phrase){
-    var vowelNumber = 0
+    var vowelNumber = 0;
     for(var i = 0; i < phrase.length; i++){
         if (phrase[i] == 'a' || phrase[i] == 'e' || phrase[i] == 'i' || phrase[i] == 'o' || phrase[i] == 'u'){
-            vowelNumber += 1
+            vowelNumber++;        
         }
     }
-    return vowelNumber
+    return vowelNumber;
 }
 
 console.log(numberOfVowels('Bom Dia'))
 
-// Função Número de vogais
+// Função Vezes por letra
 
 function numberOfTimesPerLetter(phrase,letter){
-    var timesPerLetter = 0
+    var timesPerLetter = 0;
     for (var i = 0; i < phrase.length; i++){
         if (phrase[i].toLowerCase() == letter.toLowerCase()){
-            timesPerLetter += 1
+            timesPerLetter ++
         }
     }
-    return timesPerLetter
+    return timesPerLetter;
 }
 
 console.log(numberOfTimesPerLetter('O rato roeu a roupa do rei de Roma','r'))
+
+// Função Tempo de trabalho
+
+function workTime(entryTimeHour,entryTimeMinutes,entryTimeSeconds,exitTimeHour,exitTimeMinutes,exitTimeSeconds) {
+    var totalWorkTimeHours = 0;
+    var totalWorkTimeMinutes = 0;
+    while (entryTimeHour>0) {
+        entryTimeHour --
+        entryTimeSeconds += 3600
+    }
+    while (entryTimeMinutes>0) {
+        entryTimeMinutes --
+        entryTimeSeconds += 60
+    }
+    while (exitTimeHour>0) {
+        exitTimeHour--
+        exitTimeSeconds+=3600
+    }
+    while (exitTimeMinutes>0){
+        exitTimeMinutes--
+        exitTimeSeconds+=60
+    }
+
+    var totalWorkTimeSeconds = exitTimeSeconds - entryTimeSeconds;
+
+    while (totalWorkTimeSeconds>=60){
+        if (totalWorkTimeSeconds>=3600){
+            totalWorkTimeSeconds-=3600
+            totalWorkTimeHours++
+        }
+        else{
+            totalWorkTimeMinutes++
+            totalWorkTimeSeconds-=60
+        }
+    }
+    console.log('Trabalhou: ' + totalWorkTimeHours + ' Horas, ' + totalWorkTimeMinutes + ' Minutos e ' + totalWorkTimeSeconds + ' segundos.')
+}
+
+workTime(10,0,0,18,0,0)
+
+// Função Desenha quadrado
+
+function drawSquare(height,width){
+    var squareDrawn = '';
+    var pointsInSquare = height * width;
+    var iWidth = 0;
+    for (var i = 1; i <= pointsInSquare; i++){
+        if (iWidth == width){
+            squareDrawn = squareDrawn + '\no'
+            iWidth = 1
+        }
+        else {
+            squareDrawn = squareDrawn + 'o'
+            iWidth++
+        }
+    }
+    return squareDrawn
+}
+
+console.log(drawSquare(20,10)) 
--- Ficha02/test.js
+++ Ficha02/test.js
@@ -1,1 +1,40 @@
-function workTime()
+// function workTime(entryTime,exitTime){
+//     var totalWorkTime = exitTime - entryTime
+//     return totalWorkTime
+// }
+
+// function workTime(timeWorkedSeconds){
+//     var timeWorkedMinutes = 0;
+//     var timeWorkedHours = 0;
+//     while (timeWorkedSeconds >= 60) {
+//         if (timeWorkedSeconds >= 3600) {
+//             timeWorkedSeconds -= 3600
+//             timeWorkedHours++
+//         }
+//         else {
+//             timeWorkedSeconds -= 60
+//             timeWorkedMinutes++
+//         }
+//     }
+//     console.log('Trabalhou: ' + timeWorkedHours + ' Horas, ' + timeWorkedMinutes + ' Minutos e ' + timeWorkedSeconds + 'segundos.')
+// }
+// workTime(36000)
+
+function drawSquare(height,width){
+    var squareDrawn = '';
+    var pointsInSquare = height * width;
+    var iWidth = 0
+    for (var i = 1; i <= pointsInSquare; i++){
+        if (iWidth == width){
+            squareDrawn = squareDrawn + '\no'
+            iWidth = 1
+        }
+        else {
+            squareDrawn = squareDrawn + 'o'
+            iWidth++
+        }
+    }
+    return squareDrawn
+}
+
+console.log(drawSquare(20,10)) 
