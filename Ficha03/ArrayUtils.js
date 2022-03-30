1e9362f20a4e50060c0e5b8a96c982a77f54308d
joaonunes833 <joaonunes833@hotmail.com>
2022-03-30 18:40:13 +0100

    30/03 arrayUtils

--- Ficha03/ArrayUtils.js
+++ Ficha03/ArrayUtils.js
@@ -1,112 +1,159 @@
 var utils = {
-    booleanIsEmpty: function(array){
-        if (array.length == 0){
+    booleanIsEmpty: function (array) {
+        if (array.length == 0) {
             return true;
         }
         else {
             return false;
         }
     },
-    numberMax: function(array){
-        var arrayMax = array[0];
+    numberMax: function (array) {
+        if (array.length != 0) {
+            var arrayMax = array[0];
+            for (var i = 0; i < array.length; i++) {
 
-        for (var i = 0; i < array.length; i++) {
-
-            if (arrayMax < array[i]) {
-                arrayMax =  array[i]
+                if (arrayMax < array[i]) {
+                    arrayMax = array[i]
+                }
             }
-        }
-        return arrayMax;
-    },
-    numberMin: function(array){
-        var arrayMin = array[0];
-
-        for (var i = 0; i < array.length; i++) {
-
-            if (arrayMin > array[i]) {
-                arrayMin = array[i]
-            }
-        }
-        return arrayMin;
-    },
-    numberMin: function(array){
-        var arrayMin = array[0];
-    
-        for (var i = 0; i < array.length; i++) {
-    
-            if (arrayMin > array[i]) {
-                arrayMin = array[i]
-            }
-        }
-        return arrayMin;
-    },
-    numberAverage: function(array){
-        var arrayAverage = 0
-        for (var i = 0; i < array.length; i++){
-            arrayAverage += array[i]
-        } 
-        arrayAverage = arrayAverage / array.length
-        return arrayAverage;
-    },
-    numberIndexOf: function(array,value){
-        var result;
-        if (value < array.length) {
-            return array[value];
+            return arrayMax;
         }
         else {
-            return "O elemento não existe no array.";
+            return 'Array is empty'
         }
-        
+    },
+    numberMin: function (array) {
+        if (array.length != 0) {
+            var arrayMin = array[0];
+            for (var i = 0; i < array.length; i++) {
 
-    },
-    arraySubArray: function(array, startIndex, endIndex){
-        var subArray = [];
-        for (var i = startIndex; i <= endIndex; i++){
-            subArray += array[i]
-        }
-        return subArray;
-    },
-    booleanIsSameLength: function(a1,a2){
-        if (a1.length == a2.length) {
-            return true;
+                if (arrayMin > array[i]) {
+                    arrayMin = array[i]
+                }
+            }
+            return arrayMin;
         }
         else {
-            return false;
+            return 'Array is empty'
         }
     },
-    arrayReverse: function(array){
-        reversedArray = [];
-        for (var i = 0; i < array.length; i++){
-            reversedArray.unshift(array[i])
+    numberAverage: function (array) {
+        if (array != 0) {
+            var arrayAverage = 0
+            for (var i = 0; i < array.length; i++) {
+                arrayAverage += array[i]
+            }
+            arrayAverage = arrayAverage / array.length
+            return arrayAverage;
         }
-        // for (var i = array.length - 1; i >= 0; i--){
-        //     reversedArray += array[i]
-        // }
-        return reversedArray;
+        else {
+            return 'Array is empty'
+        }
     },
-    arraySwap: function(array,index1,index2){
-        var element1 = array[index1];
-        var element2 = array[index2];
-        array[index1] = element2
-        array[index2] = element1
-        return array
-    },
-    booleanContains: function(array,value){
-        for (var i = 0;i<array.length;i++){
-            if (array[i]==value){
-                return true;
+    numberIndexOf: function (array, value) {
+        if (array != 0) {
+            var valueIndexPosition = '';
+            for (var i = 0; i < array.length; i++) {
+                if (array[i] == value) {
+                    valueIndexPosition += String(i) + ','
+                }
+            }
+            if (valueIndexPosition.length == 0) {
+                valueIndexPosition = '-1'
+                return valueIndexPosition;
+            }
+            else {
+                return 'O valor foi encontrado nos índices: ' + valueIndexPosition
             }
         }
+        else {
+            return 'Array is empty'
+        }
+
+
+
     },
-    arrayConcatenate: function(a1,a2){
-        var concatenatedArray = [];
-        for (var i = 0; i < a1.length; i++){
-            concatenatedArray += a1[i]
+    arraySubArray: function (array, startIndex, endIndex) {
+        if (array != 0) {
+            var subArray = [];
+            for (var i = startIndex; i <= endIndex; i++) {
+                subArray += array[i]
+            }
+            return subArray;
         }
-        for (var i = 0; i < a2.length; i++){
-            concatenatedArray += a2[i]
+        else {
+            return 'Array is empty'
         }
-        return concatenatedArray
+
+    },
+    booleanIsSameLength: function (a1, a2) {
+        if (a1 != 0 || a2 != 0) {
+            return a1.length == a2.length
+        }
+        else {
+            return 'Both arrays are empty'
+        }
+
+    },
+    arrayReverse: function (array) {
+        if (array != 0) {
+            reversedArray = [];
+            for (var i = 0; i < array.length; i++) {
+                reversedArray.unshift(array[i])
+            }
+            // for (var i = array.length - 1; i >= 0; i--){
+            //     reversedArray += array[i]
+            // }
+            return reversedArray;
+        }
+        else {
+            return 'Array is empty'
+        }
+
+    },
+    arraySwap: function (array, index1, index2) {
+        if (array != 0) {
+            var element1 = array[index1];
+            var element2 = array[index2];
+            array[index1] = element2
+            array[index2] = element1
+            return array
+        }
+        else {
+            return 'Array is empty'
+        }
+
+    },
+    booleanContains: function (array, value) {
+        if (array != 0) {
+            var valueExists = false;
+            for (var i = 0; i < array.length; i++) {
+                if (array[i] == value) {
+                    valueExists = true;
+                }
+            }
+            return valueExists;
+        }
+        else {
+            return 'Array is empty'
+        }
+
+    },
+    arrayConcatenate: function (a1, a2) {
+        if (a1 != 0 || a2 != 0) {
+            var concatenatedArray = [];
+            for (var i = 0; i < a1.length; i++) {
+                concatenatedArray += a1[i]
+            }
+            for (var i = 0; i < a2.length; i++) {
+                concatenatedArray += a2[i]
+            }
+            return concatenatedArray
+        }
+        else {
+            return 'Both arrays are empty'
+        }
+
     }
 
 }
