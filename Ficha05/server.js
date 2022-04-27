958dc4ed1860c6cb87e14150b86e3453832a7bd1
joaonunes833 <joaonunes833@hotmail.com>
2022-04-27 19:01:40 +0100

    Update server.js

--- Ficha05/server.js
+++ Ficha05/server.js
@@ -1,23 +1,56 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+var fs = require('fs');
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+function readFile(fileName){
+  var file = fs.readFileSync(fileName, "utf-8");
+  return file;
+}
+
+var file = readFile("./person.json");
+var fileJSON = JSON.parse(file);
+
+app.get('/', (req, res) => {
+  res.send('Hello world.');
+})
 
 app.get('/users', (req, res) => {
-  res.send('USERS')
+  res.send(fileJSON);
+})
+
+app.post('/users', (req, res) => {
+  var person = req.body;
+
+  var fileStr = readFile('./person.json');
+
+  var fileObj = JSON.parse(fileStr);
+
+  var size = Object.keys(fileObj).length
+
+  person.id = ++size;
+
+  fileObj.person5 = person;
+  res.send(JSON.stringify(fileObj));
+})
+
+app.delete('/users', (req, res) => {
+  res.send('this is a DELETE.');
+})
+
+app.put('/users', (req, res) => {
+  res.send('this is a PUT.');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
 
-// var fs = require('fs');
-
-// function readFile(fileName){
-//     var file = fs.readFileSync(fileName, "utf-8");
-//     return file;
-// }
-
-// var f = readFile("./person.json");
 
 
-// console.log(JSON.parse(f));
+
+
